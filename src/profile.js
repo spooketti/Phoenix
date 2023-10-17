af9c8b8d5a362afb9921ec9fbe43d51513533901
spooketti <jonliu168@gmail.com>
2023-10-17 08:43:44 -0700

    profiles

---
+++ src/profile.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ2GR7HQlKJBConf2Mjmqpe9KLEytsZmU",
+    authDomain: "twitterclone-aff2f.firebaseapp.com",
+    databaseURL: "https://twitterclone-aff2f-default-rtdb.firebaseio.com",
+    projectId: "twitterclone-aff2f",
+    storageBucket: "twitterclone-aff2f.appspot.com",
+    messagingSenderId: "1089185872675",
+    appId: "1:1089185872675:web:350744b5c281b8998b2a96",
+    measurementId: "G-7J6C100Q36"
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = firebase.database();
+let params = new URL(document.location).searchParams;
+let localUsernameElements = ["IntroUsername","ProfileName"]
+for(let i=0;i<localUsernameElements.length;i++)
+{
+    document.getElementById(localUsernameElements[i]).innerText = params.get("Name")
+}
+
+
