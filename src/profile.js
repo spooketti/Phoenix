972ac9355231d8f214796c2e4c505089a0c0cd0b
spooketti <jonliu168@gmail.com>
2023-10-17 09:13:13 -0700

    post butotn

--- src/profile.js
+++ src/profile.js
@@ -8,8 +8,8 @@ const firebaseConfig = {
     appId: "1:1089185872675:web:350744b5c281b8998b2a96",
     measurementId: "G-7J6C100Q36"
   };
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  const app = firebase.initializeApp(firebaseConfig);
+  //const analytics = firebase.getAnalytics(app);
   const db = firebase.database();
 let params = new URL(document.location).searchParams;
 let localUsernameElements = ["IntroUsername","ProfileName"]
