533b93a3b1aae95013d3b43cbed75c95a55feffb
spooketti <jonliu168@gmail.com>
2023-10-19 14:51:57 -0700

    ew comments (deleted)

--- src/loadpost.js
+++ src/loadpost.js
@@ -1,11 +1,3 @@
-/*<div class="Post">
-                <div class="UserHold">
-                  <img src="images/icon.png" class="PostImg">
-                <span><a href="profile.html?Name=Username">Username</a></span>
-                </div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo quibusdam nostrum ratione dignissimos fugit doloremque necessitatibus officiis libero dolor, laudantium placeat, odio expedita, provident eius sequi harum corporis consequuntur distinctio.</p>
-              </div> */
-
 const postsRef = db.ref("Posts/")
 let urlRegex = /(https?:\/\/[^\s]+)/g; //just detects links
 let PostCol = document.getElementById("Posts")
@@ -32,9 +24,7 @@ postsRef.on("child_added", function(snapshot) {
 
     let postContent = document.createElement("p")
     let postText = urlify(dbPost.message)
-    //let postSpan = document.createElement("span")
     let htmlArr = []
-    //let textSection
     for(let i=0;i<postText.length;i++)
     {
       let textSection = document.createElement(htmlArr[i])
@@ -69,34 +59,5 @@ postsRef.on("child_added", function(snapshot) {
 
 
 function urlify(text) {
- // alert(1)
   return text.split(urlRegex)
-  // or alternatively
-  // return text.replace(urlRegex, '<a href="$1">$1</a>')
-  //split into a dictionary that makes
-  //string false
-  //string false
-  //link true
-  //string false
 }
-
-/*
-   for(let i=0;i<postText.length;i++)
-    {
-      htmlArr.push("span")
-      if((postText[i] || '').split(urlRegex).length > 1)
-      {
-        htmlArr[i] = "a"
-      }
-      let textSection = document.createElement(htmlArr[i])
-      textSection.innerText = postText[i]
-      //console.log(postText[i])
-      if(htmlArr[i] == "a")
-      {
-        textSection.href = postText[i]
-        textSection.target = "_blank"
-        textSection.rel = "noopener"
-      }
-      postContent.appendChild(textSection)
-    }
-*/
