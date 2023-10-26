ede95466faf8a982a6a650b05002c41524c671d1
SriS126 <smsurapaneni@gmail.com>
2023-10-25 19:28:24 -0700

    maybe the friends button will work?

--- src/possibleusers.js
+++ src/possibleusers.js
@@ -5,27 +5,20 @@ const user_data = [
     { name: "Sidhi", profile_picture: "images/SidhiPfp.jpg", age: 12, location: "New York" },
     { name: "Gavin", profile_picture: "images/GavinPfp.jpg", age: 15, location: "Paris" }
 ];
-
 function generateUserProfiles(numProfiles) {
     const userProfiles = [];
-
     for (let i = 0; i < numProfiles; i++) {
         const randomUser = user_data[Math.floor(Math.random() * user_data.length)];
         userProfiles.push(randomUser);
     }
-
     return userProfiles;
 }
-
 document.addEventListener("DOMContentLoaded", function () {
     const generateUserButton = document.getElementById("generateUserButton");
     const userDetails = document.getElementById("userDetails");
-
     generateUserButton.addEventListener("click", function () {
-        userDetails.innerHTML = ''; 
-
+        userDetails.innerHTML = '';
         const randomProfiles = generateUserProfiles(1);
-
         randomProfiles.forEach(profile => {
             const userDiv = document.createElement("div");
             userDiv.innerHTML = `<strong>Name:</strong> ${profile.name}<br><strong>Age:</strong> ${profile.age}<br><strong>Location:</strong> ${profile.location}`;
