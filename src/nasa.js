e036adf9a60d298d84b78468dfe9921d8444fff5
spooketti <jonliu168@gmail.com>
2023-10-30 02:20:32 -0700

    comment

--- src/nasa.js
+++ src/nasa.js
@@ -2,6 +2,7 @@ var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
 var api_key = "U1Log8NeUnLqV03SBUbjcP61fEHF10YuDeUA9KGm";
 var img_type = ".png";
+//var date = "2023-09-13"
 
 //"&date="  + date
 req.open("GET", url + api_key);
